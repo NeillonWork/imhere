e3d88b2d569491bd0e358c09b3a218574a06e42d
Admin <neillonwork@gmail.com>
2024-07-15 20:29:58 -0300

    criado a pasta Home

--- App.tsx
+++ App.tsx
@@ -1,29 +1,5 @@
-import { Text, View, StyleSheet } from "react-native";
+import Home from "./src/screens/Home";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.eventName}>Nome do Evento</Text>
-      <Text style={styles.eventDate}>Sexta, 4 de novembro de 2024</Text>
-    </View>
-  );
+  return <Home />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "#131006",
-    flex: 1,
-    padding: 24,
-  },
-  eventName: {
-    color: "#FDFCFE",
-    fontSize: 24,
-    fontWeight: "bold",
-    marginTop: 38,
-  },
-  eventDate: {
-    color: "#6B6B6B",
-    marginTop: 5,
-    fontSize: 16,
-  },
-});
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#131006",
+    flex: 1,
+    padding: 24,
+  },
+  eventName: {
+    color: "#FDFCFE",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 38,
+  },
+  eventDate: {
+    color: "#6B6B6B",
+    marginTop: 5,
+    fontSize: 16,
+  },
+});
