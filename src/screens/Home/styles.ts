4c43997d7d7117645e3788e012bd006dbb38d0c8
Admin <neillonwork@gmail.com>
2024-07-15 21:03:04 -0300

    criado o botao

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -25,4 +25,16 @@ export const styles = StyleSheet.create({
     padding: 16,
     fontSize: 16,
   },
+  button: {
+    width: 56,
+    height: 56,
+    borderRadius: 5,
+    backgroundColor: "#32CF67",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    color: "#FFF",
+    fontSize: 24,
+  },
 });
